4408d544060a160d367a2c85272d6eb41152639f
sonchikgalkina <74106399+sonchikgalkina@users.noreply.github.com>
2024-09-21 17:20:34 +0300

    Update AVL_tree.h

--- associative_container/search_tree/binary_search_tree/AVL_tree/include/AVL_tree.h
+++ associative_container/search_tree/binary_search_tree/AVL_tree/include/AVL_tree.h
@@ -3,9 +3,7 @@
 
 #include <binary_search_tree.h>
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 class AVL_tree final:
     public binary_search_tree<tkey, tvalue>
 {
@@ -15,10 +13,46 @@ private:
     struct node final:
         binary_search_tree<tkey, tvalue>::node
     {
-        
-        // TODO: think about it!
-        
+
+    public:
+
+        unsigned char _height;
+
+    public:
+    
+        explicit node(tkey const &key, tvalue const &value) : binary_search_tree<tkey, tvalue>::node(key, value), _height(1)
+        {}
+        explicit node(tkey const &key, tvalue &&value)  : binary_search_tree<tkey, tvalue>::node(key, value), _height(1)
+        {}
+
+        unsigned char get_height() override
+        {
+            return _height;
+        }
+
+        void set_height(unsigned char height) override
+        {
+            _height = height;
+        }
+
     };
+
+public:
+
+    size_t get_node_size() const override
+    {
+        return sizeof(AVL_tree<tkey, tvalue>::node);
+    }
+
+    void call_node_constructor(typename binary_search_tree<tkey, tvalue>::node * raw_space, tkey const &key, tvalue const &value) const override
+    {
+        allocator::construct(reinterpret_cast<AVL_tree<tkey, tvalue>::node*>(raw_space), key, value);
+    }
+
+    void call_node_constructor(typename binary_search_tree<tkey, tvalue>::node * raw_space, tkey const &key, tvalue &&value) const override
+    {
+        allocator::construct(reinterpret_cast<AVL_tree<tkey, tvalue>::node*>(raw_space), key, std::move(value));
+    }
 
 public:
     
@@ -28,7 +62,7 @@ public:
     
     public:
         
-        size_t subtree_height;
+        size_t _subtree_height;
     
     public:
         
@@ -39,6 +73,18 @@ public:
             size_t subtree_height);
         
     };
+
+    void call_iterator_data_construct(typename binary_search_tree<tkey, tvalue>::iterator_data * raw_space, 
+        typename binary_search_tree<tkey, tvalue>::node * subtree_root, unsigned int depth) const override
+    {
+        allocator::construct(reinterpret_cast<AVL_tree<tkey, tvalue>::iterator_data*>(raw_space), depth,
+            subtree_root->key, subtree_root->value, reinterpret_cast<AVL_tree<tkey, tvalue>::node*>(subtree_root)->_height);
+    }
+
+    size_t get_iterator_data_size() const noexcept override
+    {
+        return sizeof(AVL_tree<tkey, tvalue>::iterator_data);
+    }
 
 private:
     
@@ -51,10 +97,55 @@ private:
         explicit insertion_template_method(
             AVL_tree<tkey, tvalue> *tree,
             typename binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy insertion_strategy);
-    
-    private:
-        
-        // TODO: think about it!
+
+    protected:
+
+        int get_balance_factor(typename binary_search_tree<tkey, tvalue>::node * subtree_root)
+        {
+            if (!subtree_root) return 0;
+            return (subtree_root->left_subtree ? subtree_root->left_subtree->get_height() : 0) -
+                (subtree_root->right_subtree ? subtree_root->right_subtree->get_height() : 0);
+        }
+
+        void fix_height(typename binary_search_tree<tkey, tvalue>::node * subtree_root)
+        {
+            if (!subtree_root) return;
+            node * ptr = reinterpret_cast<typename AVL_tree<tkey, tvalue>::node *>(subtree_root);
+            unsigned char left_height = ptr->left_subtree ? ptr->left_subtree->get_height() : 0;
+            unsigned char right_height = ptr->right_subtree ? ptr->right_subtree->get_height() : 0;
+            unsigned char result = (left_height > right_height ? left_height : right_height) + 1;
+            ptr->set_height(result);
+        }
+
+        void balance(std::stack<typename binary_search_tree<tkey, tvalue>::node **> &path) override
+        {
+            if (path.empty()) return;
+
+            typename binary_search_tree<tkey, tvalue>::node ** current = path.top();
+            path.pop();
+
+            int balance_factor = get_balance_factor(*current);
+
+            if (balance_factor > 1 && get_balance_factor((*current)->left_subtree) >= 0)
+                this->_tree->small_right_rotation(*current);
+            
+            if (balance_factor > 1 && get_balance_factor((*current)->left_subtree) < 0)
+                this->_tree->big_right_rotation(*current);
+            
+            if (balance_factor < -1 && get_balance_factor((*current)->right_subtree) <= 0)
+                this->_tree->small_left_rotation(*current);
+
+            if (balance_factor < -1 && get_balance_factor((*current)->right_subtree) > 0)
+                this->_tree->big_left_rotation(*current);
+
+            if (*current)
+            {
+                fix_height((*current)->left_subtree);
+                fix_height((*current)->right_subtree);
+            }
+            fix_height(*current);
+            balance(path);
+        }
         
     };
     
@@ -80,14 +171,62 @@ private:
         explicit disposal_template_method(
             AVL_tree<tkey, tvalue> *tree,
             typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy disposal_strategy);
-        
-        // TODO: think about it!
-        
+
+    protected:
+
+        int get_balance_factor(typename binary_search_tree<tkey, tvalue>::node * subtree_root)
+        {
+            if (!subtree_root) return 0;
+            return (subtree_root->left_subtree ? subtree_root->left_subtree->get_height() : 0) -
+                (subtree_root->right_subtree ? subtree_root->right_subtree->get_height() : 0);
+        }
+
+        void fix_height(typename binary_search_tree<tkey, tvalue>::node * subtree_root)
+        {
+            if (!subtree_root) return;
+            node * ptr = reinterpret_cast<typename AVL_tree<tkey, tvalue>::node *>(subtree_root);
+            unsigned char left_height = ptr->left_subtree ? ptr->left_subtree->get_height() : 0;
+            unsigned char right_height = ptr->right_subtree ? ptr->right_subtree->get_height() : 0;
+            unsigned char result = (left_height > right_height ? left_height : right_height) + 1;
+            ptr->set_height(result);
+        }
+
+        void balance(std::stack<typename binary_search_tree<tkey, tvalue>::node **> &path) override
+        {
+            if (path.empty()) return;
+
+            typename binary_search_tree<tkey, tvalue>::node ** current = path.top();
+            path.pop();
+
+            int balance_factor = get_balance_factor(*current);
+
+            if (balance_factor > 1 && get_balance_factor((*current)->left_subtree) >= 0)
+                this->_tree->small_right_rotation(*current);
+            
+            if (balance_factor > 1 && get_balance_factor((*current)->left_subtree) < 0)
+                this->_tree->big_right_rotation(*current);
+            
+            if (balance_factor < -1 && get_balance_factor((*current)->right_subtree) <= 0)
+                this->_tree->small_left_rotation(*current);
+
+            if (balance_factor < -1 && get_balance_factor((*current)->right_subtree) > 0)
+                this->_tree->big_left_rotation(*current);
+
+            if (*current)
+            {
+                fix_height((*current)->left_subtree);
+                fix_height((*current)->right_subtree);
+            }
+            fix_height(*current);
+            balance(path);
+        }
+
     };
 
 public:
     
     explicit AVL_tree(
+        std::function<int(tkey const &, tkey const &)> comparer = std::less<tkey>(),
         allocator *allocator = nullptr,
         logger *logger = nullptr,
         typename binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy insertion_strategy = binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy::throw_an_exception,
@@ -111,103 +250,139 @@ public:
     
 };
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::iterator_data::iterator_data(
     unsigned int depth,
     tkey const &key,
     tvalue const &value,
     size_t subtree_height):
-    binary_search_tree<tkey, tvalue>::iterator_data(depth, key, value)
-{
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::iterator_data::iterator_data(unsigned int, tkey const &, tvalue const &, size_t)", "your code should be here...");
-}
+    binary_search_tree<tkey, tvalue>::iterator_data(depth, key, value), _subtree_height(subtree_height)
+{}
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::insertion_template_method::insertion_template_method(
     AVL_tree<tkey, tvalue> *tree,
     typename binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy insertion_strategy):
     binary_search_tree<tkey, tvalue>::insertion_template_method(tree, insertion_strategy)
-{
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::insertion_template_method::insertion_template_method(AVL_tree<tkey, tvalue> *, typename binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy)", "your code should be here...");
-}
+{}
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::obtaining_template_method::obtaining_template_method(
-    AVL_tree<tkey, tvalue> *tree)
-{
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::obtaining_template_method::obtaining_template_method(AVL_tree<tkey, tvalue> *)", "your code should be here...");
-}
+    AVL_tree<tkey, tvalue> *tree):
+    binary_search_tree<tkey, tvalue>::obtaining_template_method(tree)
+{}
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::disposal_template_method::disposal_template_method(
     AVL_tree<tkey, tvalue> *tree,
-    typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy disposal_strategy)
-{
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::disposal_template_method::disposal_template_method(AVL_tree<tkey, tvalue> *, typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy)", "your code should be here...");
-}
+    typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy disposal_strategy):
+    binary_search_tree<tkey, tvalue>::disposal_template_method(tree, disposal_strategy)
+{}
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::AVL_tree(
+    std::function<int(tkey const &, tkey const &)> comparer,
     allocator *allocator,
     logger *logger,
     typename binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy insertion_strategy,
-    typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy disposal_strategy)
+    typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy disposal_strategy):
+    binary_search_tree<tkey, tvalue>::binary_search_tree<tkey, tvalue>(
+        new typename AVL_tree<tkey, tvalue>::insertion_template_method(this, insertion_strategy),
+        new typename AVL_tree<tkey, tvalue>::obtaining_template_method(this), 
+        new typename AVL_tree<tkey, tvalue>::disposal_template_method(this, disposal_strategy), 
+        comparer, allocator, logger)
 {
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::AVL_tree(allocator *, logger *, typename binary_search_tree<tkey, tvalue>::insertion_of_existent_key_attempt_strategy, typename binary_search_tree<tkey, tvalue>::disposal_of_nonexistent_key_attempt_strategy)", "your code should be here...");
+    if (logger) logger->debug("[AVL] Constructor\n");
 }
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::~AVL_tree() noexcept
 {
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::~AVL_tree() noexcept", "your code should be here...");
+    this->debug_with_guard("[AVL] Destructor\n");
 }
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::AVL_tree(
     AVL_tree<tkey, tvalue> const &other)
 {
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::AVL_tree(AVL_tree<tkey, tvalue> const &)", "your code should be here...");
+    this->debug_with_guard("[AVL] [START] Copy constructor\n");
+
+    if (this->_root != nullptr) clear(this->_root);
+    this->_root = copy(other._root);
+
+    if (this->_insertion_template != nullptr) delete this->_insertion_template;
+    this->_insertion_template = new AVL_tree<tkey, tvalue>::insertion_template_method(*other._insertion_template);
+    if (this->_disposal_template != nullptr) delete this->_disposal_template;
+    this->_disposal_template = new AVL_tree<tkey, tvalue>::disposal_template_method(*other._disposal_template);
+    if (this->_obtaining_template != nullptr) delete this->_obtaining_template;
+    this->_obtaining_template = new AVL_tree<tkey, tvalue>::obtaining_template_method(*other._obtaining_template);
+
+    this->debug_with_guard("[AVL] [END] Copy constructor\n");
 }
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue> &AVL_tree<tkey, tvalue>::operator=(
     AVL_tree<tkey, tvalue> const &other)
 {
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue> &AVL_tree<tkey, tvalue>::operator=(AVL_tree<tkey, tvalue> const &)", "your code should be here...");
+    this->debug_with_guard("[AVL] [START] Assignment operator\n");
+    if (this == &other)
+    {
+        this->debug_with_guard("[AVL] [END] Assignment operator\n");
+        return *this;
+    }
+    if (this->_root != nullptr) clear(this->_root);
+    this->_root = copy(other._root);
+
+    if (this->_insertion_template != nullptr) delete this->_insertion_template;
+    this->_insertion_template = new AVL_tree<tkey, tvalue>::insertion_template_method(*other._insertion_template);
+    if (this->_disposal_template != nullptr) delete this->_disposal_template;
+    this->_disposal_template = new AVL_tree<tkey, tvalue>::disposal_template_method(*other._disposal_template);
+    if (this->_obtaining_template != nullptr) delete this->_obtaining_template;
+    this->_obtaining_template = new AVL_tree<tkey, tvalue>::obtaining_template_method(*other._obtaining_template);
+
+    this->debug_with_guard("[AVL] [END] Assignment operator\n");
+    return *this;
 }
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue>::AVL_tree(
     AVL_tree<tkey, tvalue> &&other) noexcept
 {
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue>::AVL_tree(AVL_tree<tkey, tvalue> &&) noexcept", "your code should be here...");
+    this->debug_with_guard("[AVL] [START] Move constructor\n");
+    if (this->_root != nullptr) this->clear(this->_root);
+    this->_root = std::exchange(other._root, nullptr);
+
+    if (this->_insertion_template != nullptr) delete this->_insertion_template;
+    this->_insertion_template = std::exchange(other._insertion_template, nullptr);
+    if (this->_disposal_template != nullptr) delete this->_disposal_template;
+    this->_disposal_template = std::exchange(other._disposal_template, nullptr);
+    if (this->_obtaining_template != nullptr) delete this->_obtaining_template;
+    this->_obtaining_template = std::exchange(other._obtaining_template, nullptr);
+
+    this->debug_with_guard("[AVL] [END] Move constructor\n");
 }
 
-template<
-    typename tkey,
-    typename tvalue>
+template<typename tkey, typename tvalue>
 AVL_tree<tkey, tvalue> &AVL_tree<tkey, tvalue>::operator=(
     AVL_tree<tkey, tvalue> &&other) noexcept
 {
-    throw not_implemented("template<typename tkey, typename tvalue> AVL_tree<tkey, tvalue> &AVL_tree<tkey, tvalue>::operator=(AVL_tree<tkey, tvalue> &&) noexcept", "your code should be here...");
+    this->debug_with_guard("[AVL] [START] Move operator\n");
+    if (this == &other)
+    {
+        this->debug_with_guard("[AVL] [END] Move operator\n");
+        return *this;
+    }
+    if (this->_root != nullptr) clear(this->_root);
+    this->_root = std::exchange(other._root, nullptr);
+
+    if (this->_insertion_template != nullptr) delete this->_insertion_template;
+    this->_insertion_template = std::exchange(other._insertion_template, nullptr);
+    if (this->_disposal_template != nullptr) delete this->_disposal_template;
+    this->_disposal_template = std::exchange(other._disposal_template, nullptr);
+    if (this->_obtaining_template != nullptr) delete this->_obtaining_template;
+    this->_obtaining_template = std::exchange(other._obtaining_template, nullptr);
+    this->debug_with_guard("[AVL] [END] Move operator\n");
+    return *this;
 }
 
 #endif //MATH_PRACTICE_AND_OPERATING_SYSTEMS_AVL_TREE_H
